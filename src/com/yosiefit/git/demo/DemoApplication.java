8e839017eee47650ab3268ed3af4c6e58107bf6e
Yosief <yosie@DESKTOP-IMGAKRV.lan>
2023-06-20 18:53:53 -0700

    First commit in the GitDemoProject_V1 setup....

---
+++ src/com/yosiefit/git/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.yosiefit.git.demo;
+
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		boolean hungry = true;
+		if(hungry == true) {
+			System.out.println("Eat something before your starve.......");
+		}else {
+			System.out.println("Do some exercise to burn your calories......");
+		}
+
+	}
+
+}
