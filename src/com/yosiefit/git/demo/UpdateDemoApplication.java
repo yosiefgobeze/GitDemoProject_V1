3ff22a3e1de3b0f2a7dd817f796e156a9fe8d67d
yosie <yosie@DESKTOP-T5E57B5.lan>
2023-06-20 23:38:43 -0700

    Updates on the GitDemoProject_V1 as a collaborator

---
+++ src/com/yosiefit/git/demo/UpdateDemoApplication.java
@@ -0,0 +1,17 @@
+package com.yosiefit.git.demo;
+
+public class UpdateDemoApplication {
+
+	public static void main(String[] args) {
+		// code added by the collaborator
+		String name = "Noah";
+		if(name == "Noah") {
+			System.out.println("Just go and eat");
+		}else {
+			System.out.println("Ask someone");
+		}
+		
+
+	}
+
+}
