93b835f8d5bd4b87ca7b34ddc491bc423b46bd4a
yosie <yosie@DESKTOP-T5E57B5.lan>
2023-06-21 10:47:20 -0700

    Array basics in java

--- src/com/yosiefit/git/demo/DemoApplication.java
+++ src/com/yosiefit/git/demo/DemoApplication.java
@@ -1,15 +1,22 @@
 package com.yosiefit.git.demo;
-
+/*
+ * An array is a collection of variables of the same data type
+ * An array is an object in java
+ * Array have a fixed size
+ * An array variable is a reference object that points to the array object
+ */
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		boolean hungry = true;
-		if(hungry == true) {
-			System.out.println("Eat something before your starve.......");
-		}else {
-			System.out.println("Do some exercise to burn your calories......");
-		}
-
+		// basic array in java
+		
+		int[] numbers; // the array is null
+		numbers = new int[5];
+		numbers[0] = 5;
+		numbers[1] = 3;
+		numbers[2] = 4;
+		numbers[3] = 2;
+		numbers[4] = 6;
+		
 	}
-
 }
